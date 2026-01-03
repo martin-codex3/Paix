c999781e85a9d45d6050905914d1977cbf9c3501
martin-codex3 <martinsilungwe12@gmail.com>
2026-01-03 05:57:22 +0200

    adding project services

--- Backend/src/services/projects/project_services.py
+++ Backend/src/services/projects/project_services.py
@@ -1,15 +1,19 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.models.projects.project_model import ProjectModel
-from src.schemas.products.product_schema import CreateProductSchema
+from sqlmodel import select, desc
+from src.schemas.products.product_schema import (CreateProjectSchema,
+                                                 FetchProjectSchema)
 
 
 class ProjectService:
     """the class will hold all the database functionality"""
-    async def create_new_project(self, session: AsyncSession, project_data: CreateProductSchema):
+    async def create_new_project(self, session: AsyncSession, project_data: CreateProjectSchema):
         pass
 
     async def get_all_projects(self, session: AsyncSession):
-        pass
+        statement = select(ProjectModel).order_by(desc(ProjectModel.project_created_at))
+        results = await session.exec(statement)
+        return results.all()
 
     async def show_single_product(self, session: AsyncSession):
         pass
