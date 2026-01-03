2117a51a9e82ed96e2d5b27e120a955034279810
martin-codex3 <martinsilungwe12@gmail.com>
2026-01-03 09:56:51 +0200

    adding project routes

--- Backend/src/connection/app_connections.py
+++ Backend/src/connection/app_connections.py
@@ -3,6 +3,8 @@ from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from src.config.app_config import AppConfig
+from sqlalchemy.orm.session import sessionmaker
+
 
 # we will attempt to create the database engine here
 async_database_engine = AsyncEngine(
@@ -19,3 +21,16 @@ async def database_init():
             await connection.run_sync(SQLModel.metadata.create_all)
         else:
             connection.close()
+
+
+# we will the session maker to get the session here
+# we will use this as the dependency to get the global app session
+async def app_session():
+    Session = sessionmaker(
+        bind=async_database_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with Session() as session:
+        yield session
--- Backend/src/routes/projects/project_routes.py
+++ Backend/src/routes/projects/project_routes.py
@@ -1,8 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
+from fastapi.params import Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.services.projects.project_services import ProjectService
+from src.connection.app_connections import app_session
+
 
 project_routes = APIRouter()
+project_service = ProjectService()
 
 # all the routes for the projects here
-@project_routes.get("/")
-async def index():
-    return {"message": "all the routes for the projects here"}
+@project_routes.get("/", status_code=status.HTTP_200_OK)
+async def index(session: AsyncSession = Depends(app_session)):
+    all_projects = await project_service.get_all_projects(
+        session = session
+    )
+
+    if all_projects is not None:
+        return all_projects
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail={
+            "message": "error fetching records"
+        })
+
+
+@project_routes.post("/", status_code=status.HTTP_201_CREATED)
+async def store(session: AsyncSession = Depends(app_session)):
+    pass
