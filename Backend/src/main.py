ee73dbaa31f2dcfb8e5f5c19835c97978f03fa9e
martin-codex3 <martinsilungwe12@gmail.com>
2026-01-03 04:32:26 +0200

    adding database connections

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from src.routes.projects.project_routes import project_routes
 
+# the version for the api
+app_api_version = "v1"
 
-app = FastAPI()
+# the main app entry point here
+app = FastAPI(
+    version=app_api_version,
+    title="App Backend",
+    description="The backend will include all "
+                "the functionality to handle the different app operations"
+)
 
-app.get("/")
-async def root():
-    return {"message": "we will start working here"}
+# registering the routes here
+app.include_router(
+    router=project_routes,
+    prefix="/api/products",
+    tags=["products"]
+)
--- Backend/src/routes/projects/project_routes.py
+++ Backend/src/routes/projects/project_routes.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 
-
 project_routes = APIRouter()
 
-
 # all the routes for the projects here
-project_routes.get("/")
+@project_routes.get("/")
 async def index():
     return {"message": "all the routes for the projects here"}
