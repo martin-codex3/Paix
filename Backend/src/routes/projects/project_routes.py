8cc84f6b2037c59473bef9ab429c071931edec19
martin-codex3 <martinsilungwe12@gmail.com>
2026-01-06 15:43:30 +0200

    working on project routes

--- Backend/src/routes/projects/project_routes.py
+++ Backend/src/routes/projects/project_routes.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, status, HTTPException
 from fastapi.params import Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
+from starlette.responses import JSONResponse
+
+from src.schemas.products.product_schema import CreateProjectSchema
 from src.services.projects.project_services import ProjectService
 from src.connection.app_connections import app_session
 
@@ -24,5 +27,41 @@ async def index(session: AsyncSession = Depends(app_session)):
 
 
 @project_routes.post("/", status_code=status.HTTP_201_CREATED)
-async def store(session: AsyncSession = Depends(app_session)):
-    pass
+async def store(project_data: CreateProjectSchema, session: AsyncSession = Depends(app_session)):
+    new_project = await project_service.create_new_project(
+        session = session,
+        project_data = project_data
+    )
+
+    if new_project is not None:
+        return JSONResponse(
+            status_code=status.HTTP_201_CREATED,
+            content={
+                "message": "Project created successfully",
+            }
+        )
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={
+                "message": "failed to create the project"
+            }
+        )
+
+
+# getting a single project here
+@project_routes.get("/{project_id}", status_code=status.HTTP_200_OK)
+async def show(project_id: int, session: AsyncSession = Depends(app_session)):
+    single_project = await project_service.show_single_product(
+        project_id = project_id,
+        session = session
+    )
+
+    if single_project is not None:
+        return single_project
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Failed to fetch the record with that id"
+        )
--- Backend/src/services/projects/project_services.py
+++ Backend/src/services/projects/project_services.py
@@ -2,24 +2,51 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from src.models.projects.project_model import ProjectModel
 from sqlmodel import select, desc
 from src.schemas.products.product_schema import (CreateProjectSchema,
-                                                 FetchProjectSchema)
+                                                 FetchProjectSchema,
+                                                 UpdateProjectSchema)
 
 
 class ProjectService:
     """the class will hold all the database functionality"""
     async def create_new_project(self, session: AsyncSession, project_data: CreateProjectSchema):
-        pass
+        new_project_dict = project_data.model_dump()
+        new_project = ProjectModel(**new_project_dict)
+        if new_project is not None:
+            session.add(new_project)
+            await session.commit()
+            return new_project
+        else:
+            return None
+
 
     async def get_all_projects(self, session: AsyncSession):
         statement = select(ProjectModel).order_by(desc(ProjectModel.project_created_at))
         results = await session.exec(statement)
         return results.all()
 
-    async def show_single_product(self, session: AsyncSession):
-        pass
+    async def show_single_product(self, project_id: int, session: AsyncSession):
+        statement = select(ProjectModel).where(ProjectModel.project_id == project_id)
+        results = await session.exec(statement)
+        return results.first()
 
-    async def update_project(self, session: AsyncSession, project_id: int):
-        pass
+    async def update_project(self, project_data: UpdateProjectSchema, session: AsyncSession, project_id: int):
+        project = await self.show_single_product(
+            project_id = project_id,
+            session = session
+        )
+
+        if project is not None:
+            project_to_update = project_data.model_dump()
+            for keys, values in project_to_update.items():
+                setattr(project, keys, values)
+                session.add(project_to_update)
+                await session.commit()
+
+            return project_to_update
+        else:
+            return None
+
+
 
     async def delete_project(self, session: AsyncSession, project_id: int):
         pass
