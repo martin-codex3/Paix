f96d44d014c4a95d72756065d3529f7ffefaa3e9
martin-codex3 <martinsilungwe12@gmail.com>
2026-01-06 10:40:59 +0200

    adding jwt decoding

--- Backend/dependencies/jwt_dependency.py
+++ Backend/dependencies/jwt_dependency.py
@@ -1,6 +1,6 @@
 import jwt
 from sqlalchemy import false
-
+import logging
 from src.config.app_config import AppConfig
 from datetime import datetime, timedelta, timezone
 
@@ -23,3 +23,19 @@ def encode_jwt_token(user: dict, refresh: bool = False) -> str:
     )
 
     return jwt_token
+
+
+# we will attempt to decode the token here
+def decode_jwt_token(jwt_token: str):
+    try:
+        decoded_token = jwt.decode(
+            jwt=jwt_token,
+            key=key,
+            algorithms=[algorithm]
+        )
+
+        return decoded_token
+
+    except jwt.PyJWTError as error:
+        logging.error(error)
+        return None
